2d5fa0a8f502d0b102d32466ee3031e56565e2e7
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-24 17:34:38 +0500

    fixed loadUserByUsername

--- auth-service/src/main/java/ru/aesaq/messengerx/authservice/service/MyUserDetailsService.java
+++ auth-service/src/main/java/ru/aesaq/messengerx/authservice/service/MyUserDetailsService.java
@@ -1,12 +1,22 @@
 package ru.aesaq.messengerx.authservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.aesaq.messengerx.authservice.entity.User;
+import ru.aesaq.messengerx.authservice.repository.UserRepository;
+import ru.aesaq.messengerx.authservice.security.MyUserDetails;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findUserByUsername(username);
+        return new MyUserDetails(user);
     }
 }
