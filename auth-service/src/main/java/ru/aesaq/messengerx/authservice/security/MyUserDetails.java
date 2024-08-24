8a35fd24217ed70863462f7ae0665d866ba7d26d
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-24 17:33:39 +0500

    fix getUsername, getPassword; added constructor

--- auth-service/src/main/java/ru/aesaq/messengerx/authservice/security/MyUserDetails.java
+++ auth-service/src/main/java/ru/aesaq/messengerx/authservice/security/MyUserDetails.java
@@ -2,10 +2,17 @@ package ru.aesaq.messengerx.authservice.security;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import ru.aesaq.messengerx.authservice.entity.User;
 
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
+    private User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -13,11 +20,11 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 }
