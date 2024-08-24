f155cdf7aa135c7d130471762a26d3087ff17f81
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-24 17:19:16 +0500

    initial MyUserDetails, MyUserDetailsService

---
+++ auth-service/src/main/java/ru/aesaq/messengerx/authservice/security/MyUserDetails.java
@@ -0,0 +1,23 @@
+package ru.aesaq.messengerx.authservice.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
