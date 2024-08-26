2faf7e1c154945c9a79e411fbb8f7c8a2723b01b
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-26 19:51:16 +0500

    refactoring

--- auth-service/src/main/java/ru/aesaq/messengerx/authservice/controller/UserController.java
+++ auth-service/src/main/java/ru/aesaq/messengerx/authservice/controller/UserController.java
@@ -1,95 +1,37 @@
 package ru.aesaq.messengerx.authservice.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.aesaq.messengerx.authservice.entity.User;
-import ru.aesaq.messengerx.authservice.repository.UserRepository;
+import ru.aesaq.messengerx.authservice.service.AuthService;
+import ru.aesaq.messengerx.authservice.service.AuthenticationRequest;
 import ru.aesaq.messengerx.authservice.util.JwtUtil;
 
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
 
-    public UserController(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public UserController(AuthService authService, JwtUtil jwtUtil) {
+        this.authService = authService;
         this.jwtUtil = jwtUtil;
     }
 
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-            );
-        } catch (AuthenticationException e) {
-            String errorResponse = "Invalid username or password";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }
-
-        final String jwt = jwtUtil.generateToken(authenticationRequest.getUsername());
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        return authService.login(authenticationRequest);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        if (user.getUsername() == null || user.getPassword() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-        if (userRepository.existsByUsername(user.getUsername())) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        String hashedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(hashedPassword);
-        User registeredUser = userRepository.save(user);
-        return ResponseEntity.ok(registeredUser);
-    }
-}
-
-class AuthenticationRequest {
-    private String username;
-    private String password;
-
-    public String getUsername() {
-        return username;
+    public ResponseEntity<?> register(@RequestBody User user) {
+        return authService.register(user);
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-
-class AuthenticationResponse {
-    private String jwt;
-
-    public AuthenticationResponse(String jwt) {
-        this.jwt = jwt;
-    }
-
-    public String getJwt() {
-        return jwt;
+    @GetMapping("/my-info")
+    public String getInfo(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.substring(7);
+        return jwtUtil.extractUsername(token) + " " + jwtUtil.extractExpiration(token);
     }
 }
--- auth-service/src/main/java/ru/aesaq/messengerx/authservice/service/AuthService.java
+++ auth-service/src/main/java/ru/aesaq/messengerx/authservice/service/AuthService.java
@@ -1,27 +1,73 @@
 package ru.aesaq.messengerx.authservice.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.aesaq.messengerx.authservice.entity.User;
 import ru.aesaq.messengerx.authservice.repository.UserRepository;
+import ru.aesaq.messengerx.authservice.util.JwtUtil;
+import ru.aesaq.messengerx.authservice.validator.AuthValidator;
 
 @Service
 public class AuthService {
-    private final UserRepository userRepository;
-
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, AuthValidator authValidator, PasswordEncoder passwordEncoder, JwtUtil jwtUtil, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.authValidator = authValidator;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
     }
 
-    public User register(User user) {
-        return userRepository.save(user);
-    }
+    private final UserRepository userRepository;
+    private final AuthValidator authValidator;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
 
-    public User login(String username, String password) {
-        User user = userRepository.findUserByUsername(username);
-        if (user != null && password.equals(user.getPassword())) {
-            return user;
-        } else {
-            return null;
+    public ResponseEntity<?> register(User user) {
+        String validateUsernameResult = authValidator.validateUsername(user.getUsername());
+        String validatePasswordResult = authValidator.validatePassword(user.getPassword());
+        if (validateUsernameResult != "ok") {
+            return ResponseEntity.badRequest().body(validateUsernameResult);
         }
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body("username already exists");
+        }
+        if (validatePasswordResult != "ok") {
+            return ResponseEntity.badRequest().body(validatePasswordResult);
+        }
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        User registeredUser = userRepository.save(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    public ResponseEntity<?> login(AuthenticationRequest authenticationRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (Exception e) {
+            String errorResponse = "Invalid username or password";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        final String jwt = jwtUtil.generateToken(authenticationRequest.getUsername());
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
+
+class AuthenticationResponse {
+    private String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
     }
 }
