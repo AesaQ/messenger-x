ac544aa4c086565912f053d35519fd3547faf5eb
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-27 14:02:21 +0500

    initial commit learning-service

---
+++ learning-service/src/test/java/ru/AesaQ/messenger_x/learning_service/LearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.AesaQ.messenger_x.learning_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "messenger-x"
 
 include("auth-service")
+include("learning-service")
 include("common")
