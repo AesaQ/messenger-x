1313d07fb81e57ba3b14bf7b621c707c28ddbea1
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-28 15:05:51 +0500

    added createCard()

--- learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/service/CardService.java
+++ learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/service/CardService.java
@@ -1,6 +1,7 @@
 package ru.AesaQ.messenger_x.learning_service.service;
 
 import org.springframework.stereotype.Service;
+import ru.AesaQ.messenger_x.learning_service.entity.Card;
 import ru.AesaQ.messenger_x.learning_service.repository.CardRepository;
 
 @Service
@@ -11,5 +12,16 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
 
+    public void createCard(Card cardWithoutId) {
+        Card card = new Card();
+        card.setCreater(cardWithoutId.getCreater());
+        card.setUnderStudy(cardWithoutId.getUnderStudy());
+        card.setAnswer(cardWithoutId.getAnswer());
+        card.setExample1(cardWithoutId.getExample1());
+        card.setExample2(cardWithoutId.getExample2());
+        card.setExample3(cardWithoutId.getExample3());
+
+        cardRepository.save(card);
+    }
 
 }
