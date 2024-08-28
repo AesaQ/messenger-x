7ddb67c4563c5e67fcebc1a2a7c8e8e11fc46a86
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-28 15:07:15 +0500

    added @RequestMapping

--- learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/CardController.java
+++ learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/CardController.java
@@ -1,15 +1,16 @@
 package ru.AesaQ.messenger_x.learning_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.AesaQ.messenger_x.learning_service.service.CardService;
 
 @RestController
+@RequestMapping("/api/card")
 public class CardController {
     private final CardService cardService;
 
     public CardController(CardService cardService) {
         this.cardService = cardService;
     }
-
 
 }
