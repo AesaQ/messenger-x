a2bb5c813cd4b5037ea3a11bcf152cb23c728d34
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-28 15:14:56 +0500

    added createCard

--- learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/CardController.java
+++ learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/CardController.java
@@ -1,7 +1,10 @@
 package ru.AesaQ.messenger_x.learning_service.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.AesaQ.messenger_x.learning_service.entity.Card;
 import ru.AesaQ.messenger_x.learning_service.service.CardService;
 
 @RestController
@@ -13,4 +16,8 @@ public class CardController {
         this.cardService = cardService;
     }
 
+    @PostMapping("/create")
+    public String createCard(@RequestBody Card card) {
+        return cardService.createCard(card);
+    }
 }
