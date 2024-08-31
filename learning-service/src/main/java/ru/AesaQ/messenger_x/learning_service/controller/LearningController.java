e9ae0db5b11b9b1b18324fc97b637958f91ccfd1
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-08-31 14:52:01 +0500

    added putCards() method

--- learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/LearningController.java
+++ learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/controller/LearningController.java
@@ -1,14 +1,14 @@
 package ru.AesaQ.messenger_x.learning_service.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.AesaQ.messenger_x.learning_service.entity.Card;
 import ru.AesaQ.messenger_x.learning_service.service.RecommendationService;
 import ru.AesaQ.messenger_x.learning_service.util.JwtUtil;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/learn")
@@ -31,6 +31,24 @@ public class LearningController {
             return ResponseEntity.badRequest().body("you are not logged in");
         }
         return ResponseEntity.ok(recommendationService.takeCards(15, username));
+    }
+
+    @PostMapping("/put-cards")
+    public ResponseEntity<?> putCards(@RequestHeader("Authorization") String token, @RequestBody List<Card> cards) {
+        token = token.replace("Bearer ", "");
+        String username;
+        try {
+            username = jwtUtil.extractUsername(token);
+            for (Card card : cards) {
+                if (!username.equals(card.getCreator())) {
+                    throw new AccessDeniedException("There are cards in the list that you don't have access to");
+                }
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        recommendationService.putCards(cards);
+        return ResponseEntity.ok().body("ok");
     }
 
     @GetMapping("/my-info")
