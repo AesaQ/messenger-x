1cf2a5de309a1dab45e473275067d8cbb91bdcbf
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-10-06 18:12:55 +0500

    refactoring

--- learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/util/JwtUtil.java
+++ learning-service/src/main/java/ru/AesaQ/messenger_x/learning_service/util/JwtUtil.java
@@ -2,12 +2,9 @@ package ru.AesaQ.messenger_x.learning_service.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 @Component
@@ -33,17 +30,6 @@ public class JwtUtil {
 
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
-    }
-
-    public String generateToken(String username) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, username);
-    }
-
-    private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
     public Boolean validateToken(String token, String username) {
