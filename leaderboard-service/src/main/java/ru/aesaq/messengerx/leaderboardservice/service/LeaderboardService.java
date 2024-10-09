1e4db1f66fecfbdc77e914513bd78f7b37f19362
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-10-09 20:22:21 +0500

    added LeaderboardService

--- leaderboard-service/src/main/java/ru/aesaq/messengerx/leaderboardservice/service/LeaderboardService.java
+++ leaderboard-service/src/main/java/ru/aesaq/messengerx/leaderboardservice/service/LeaderboardService.java
@@ -1,4 +1,24 @@
 package ru.aesaq.messengerx.leaderboardservice.service;
 
+import org.springframework.stereotype.Service;
+import ru.aesaq.messengerx.leaderboardservice.entity.Card;
+import ru.aesaq.messengerx.leaderboardservice.repository.CardRepository;
+
+import java.util.List;
+
+@Service
 public class LeaderboardService {
+    private final CardRepository cardRepository;
+
+    public LeaderboardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public List<Object[]> getLeaderboardByCardsQuantity() {
+        return cardRepository.findTop30CreatorsByQuantityCards();
+    }
+
+    public List<Card> getCardsByCreator(String creator) {
+        return cardRepository.findCardsByCreator(creator);
+    }
 }
