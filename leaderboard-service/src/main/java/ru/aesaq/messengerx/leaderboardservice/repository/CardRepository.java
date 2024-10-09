abf4eeeada9d89ac0eecc897c54a5e06e049ac01
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-10-09 20:22:03 +0500

    added CardRepository

--- leaderboard-service/src/main/java/ru/aesaq/messengerx/leaderboardservice/repository/CardRepository.java
+++ leaderboard-service/src/main/java/ru/aesaq/messengerx/leaderboardservice/repository/CardRepository.java
@@ -1,8 +1,17 @@
 package ru.aesaq.messengerx.leaderboardservice.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ru.aesaq.messengerx.leaderboardservice.entity.Card;
+
+import java.util.List;
 
 @Repository
-public class CardRepository {
+public interface CardRepository extends JpaRepository<Card, Long> {
+    @Query("SELECT c.creator, COUNT(c.creator) AS count FROM Card c GROUP BY c.creator ORDER BY count DESC LIMIT 30")
+    List<Object[]> findTop30CreatorsByQuantityCards();
+
+    List<Card> findCardsByCreator(String creator);
 }
