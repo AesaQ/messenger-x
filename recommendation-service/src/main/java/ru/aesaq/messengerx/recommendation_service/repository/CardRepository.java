184b04ff8c96bb27b0c9279517860a77bd98bd17
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-10-13 21:47:11 +0500

    added CardRepository

---
+++ recommendation-service/src/main/java/ru/aesaq/messengerx/recommendation_service/repository/CardRepository.java
@@ -0,0 +1,17 @@
+package ru.aesaq.messengerx.recommendation_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.aesaq.messengerx.recommendation_service.entity.Card;
+
+import java.util.ArrayList;
+
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+    ArrayList<Card> getCardsByCreator(String creator);
+
+    void removeById(Long id);
+
+    Card getCardById(Long id);
+}
